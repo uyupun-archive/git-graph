47a722231471bad60c65b28cf2c0961237960a2c
Kazuki Tobita <kazukiti201@gmail.com>
2018-03-20 23:05:14 +0900

    update index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,12 +2,12 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Hello Vue</title>
-        <link rel="stylesheet" href="css/app.css"/>
-        <script type="text/javascript">
-            window.Laravel = window.Laravel || {};
-            window.Laravel.csrfToken = "{{csrf_token()}}";
-        </script>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>GitGraph</title>
+        <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+        <link rel="stylesheet" href="{{ url('/css/app.css') }}">
     </head>
     <body>
         <div id="app">
