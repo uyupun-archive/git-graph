8aaf0ead896f08923b9c44a8486d2013c5fd9b53
Kazuki Tobita <kazukiti201@gmail.com>
2018-03-20 22:47:03 +0900

    GitHub grasses scraping

--- app/Http/Controllers/GrassesController.php
+++ app/Http/Controllers/GrassesController.php
@@ -3,10 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Goutte;
 
 class GrassesController extends Controller
 {
     public function get(Request $request) {
-        return response('hoge', '200');
+
+        $crawler = Goutte::request('GET', 'https://github.com/' . $request->name);
+        $response = $crawler->filter('.day')->each(function ($element) {
+            return $element->attr('data-count');
+        });
+
+        return response($response, '200');
     }
 }
