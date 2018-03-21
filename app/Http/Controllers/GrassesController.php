93fc4c47a75492935967cfedb05cf8414c8daece
Kazuki Tobita <kazukiti201@gmail.com>
2018-03-22 01:07:39 +0900

    create backend validation

--- app/Http/Controllers/GrassesController.php
+++ app/Http/Controllers/GrassesController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Validator;
 use Goutte;
 
 class GrassesController extends Controller
 {
     public function get(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required | min: 1 | max: 39 | string | regex:/^[a-zA-Z0-9]+$/'
+        ]);
+
+        if ($validator->fails()) {
+            return response('Forbidden', '403');
+        }
 
         $crawler = Goutte::request('GET', 'https://github.com/' . $request->name);
         $response = $crawler->filter('.day')->each(function ($element) {
